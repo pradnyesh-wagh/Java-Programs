4b9123871362421413ad1d68b381b01ebdd55d0d
pradnyesh-wagh <pswagh420@gmail.com>
2022-02-22 17:17:06 +0530

    ADDED: java program for CreaditDebit

---
+++ CreditDebit.java
@@ -0,0 +1,11 @@
+class CreditDebit
+{
+	public static void main(String args[])
+	{	
+		String Msg1="Dear xxx123,your Acc has been DEBITED RS. 500 on Date. 11/11/2020";
+		System.out.println("Msg1="+Msg1);
+		String Msg2="Dear xxx123,your Acc has been CREDITED RS. 700 on Date. 13/11/2020";
+		System.out.println("Msg2="+Msg2);
+		
+	}
+}
